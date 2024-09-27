357c02b0e29ed94926956b4d2d8be5a94627ff10
Muhammad Inam <nudmaufon@gmail.com>
2024-09-27 22:23:32 +0500

    fetch-JSON-Data-old-method

--- reactpart5/TanStack-Query/src/pages/FetchOld.jsx
+++ reactpart5/TanStack-Query/src/pages/FetchOld.jsx
@@ -1,33 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import apiData from '../API/api'
+import React, { useEffect, useState } from "react";
+import apiData from "../API/api";
 
 const FetchOld = () => {
-    const [dataApi, setDataApi] = useState([])
+  const [dataApi, setDataApi] = useState([]);
 
-    const getDataApi = async () => {
-        const response = await apiData()
-        console.log(response.data)
-        setDataApi(response.data)
+  const getDataApi = async () => {
+    try {
+      const response = await apiData();
+      console.log(response.data);
+      setDataApi(response.data);
+    } catch (error) {
+      console.log(error.message);
     }
-    useEffect(( ) => {
-        getDataApi()
-    }, [])
+  };
+  useEffect(() => {
+    getDataApi();
+  }, []);
 
   return (
     <div>
-      {
-
-        dataApi.slice(0, 10).map((item, index) => {
-        
-            return (
-                <div key={index}>
-                    {item.title}
-                </div>
-            )
-        })
-      }
+      {dataApi.slice(0, 10).map((item, index) => {
+        return <div key={index}>{item.title}</div>;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default FetchOld
+export default FetchOld;
