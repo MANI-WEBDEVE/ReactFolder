432562da136876b7d03a0a33ed892371274c64f6
Muhammad Inam <149400016+MANI-WEBDEVE@users.noreply.github.com>
2024-10-10 18:03:38 +0000

    pagination-TanStack

--- reactpart5/TanStack-Query/src/API/api.jsx
+++ reactpart5/TanStack-Query/src/API/api.jsx
@@ -1,4 +1,5 @@
 import axios, {AxiosError} from 'axios'
+import { useEffect } from 'react'
 
 const fetchApi = axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com'
@@ -13,6 +14,16 @@ export default apiData
 export const fetchIndvdualData = async (id) => {
   try {
     const response = await fetchApi.get(`/posts/${id}`)
+    const data = await response.data;
+    return await data
+  } catch (error) {
+    console.log(error)
+  }
+}
+export const paginationWeb = async (pageNumber) => {
+  console.log(pageNumber)
+  try{
+    const response = await fetchApi.get(`/posts?_start=${pageNumber}&_limit=3`)
     const data = await response.data;
     return await data
   } catch (error) {
